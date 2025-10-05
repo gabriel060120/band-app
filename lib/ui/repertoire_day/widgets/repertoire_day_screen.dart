0fc765c268e76702b21582770f2c194b1dfdef08
Gabriel Patrocinio <gabriel060120@gmail.com>
2025-10-04 20:25:51 -0400

    feat: init repertoire day screen

---
+++ lib/ui/repertoire_day/widgets/repertoire_day_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RepertoireDayScreen extends StatefulWidget {
+  const RepertoireDayScreen({super.key});
+
+  @override
+  State<RepertoireDayScreen> createState() => _RepertoireDayScreenState();
+}
+
+class _RepertoireDayScreenState extends State<RepertoireDayScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return IndexedStack();
+  }
+}
